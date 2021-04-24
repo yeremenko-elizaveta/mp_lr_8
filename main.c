e79ac239fb4de377ab280fe97b6a779ef5fff01b
barymova_elizaveta <elizaveta.barymova.00@mail.ru>
2021-04-25 00:35:00 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,163 @@
+#include "mcu_support_package/inc/stm32f10x.h"
+#include <math.h>
+#define SET_SIZE( f ) (f / freqIntr_mcs )
+
+static volatile uint32_t count = 0;  // Счётчик прерываний
+static const uint16_t period = 72 * 6;
+
+void SysTick_Handler (void) {
+    count++;
+}
+
+static const uint32_t freqIntr_mcs = 6;    // Период вызова прерываний в мкс
+
+// SET_SIZE( f ) - кол-во значащих эл-тов массива значений синуса для каждого звука. 
+// f - период звука в мкс. Округлён так, чтобы делился на 6 мкс - частоту прерываний
+static const uint32_t sizeSinArr[8] = {SET_SIZE( 954 ), SET_SIZE( 852 ), SET_SIZE( 756 ), SET_SIZE( 714 ),
+                                       SET_SIZE( 636 ), SET_SIZE( 582 ), SET_SIZE( 504 ), SET_SIZE( 480 )};
+
+// Массив значений синусов
+static uint16_t sinArr[8][SET_SIZE( 954 )] = {0};
+
+// Значение пи с точностью до 20-и знаков после запятой
+static const double Pi = 3.14159265358979323846;
+
+// Заполняет массив значений синуса
+static void fillSin(uint16_t sinArr[][SET_SIZE( 954 )], const uint32_t * sizeSinX) {
+    for (uint32_t i = 0; i < 8; i++) {
+        for (uint32_t j = 0; j < sizeSinX[i]; j++) {
+            sinArr[i][j] = (sin((Pi * 2.0 * j / sizeSinX[i])) + 1.0) / 2 * period;  
+        }
+    }
+}
+
+static void play(uint32_t numBut) {
+    // Массив для сохранения в массивах синусов индексов, которые воспроизвели в последний раз
+    uint32_t index[8] = {0};
+    // Пока нажата данная комбинация кнопок, играем
+    while ((((numBut & 0x7F) << 1) == (GPIO_ReadInputData(GPIOA) & 0xFE)) && 
+           (((GPIO_ReadInputData(GPIOC) & GPIO_Pin_4) << 3) == (numBut & 0x80))) {
+        uint32_t prev = count;
+        uint32_t countBut = 0;
+        
+        // Звук, который надо проиграть
+        double sound = 0;
+        
+        // Проверяем коды всех кнопок. Если кнопки нажаты,
+        // складываем значения  
+        for (uint32_t i = 0; i < 8; i++) {
+            if (numBut & (1 << i)) {
+                sound += sinArr[i][index[i]];
+                index[i] = (index[i] + 1) % sizeSinArr[i];
+                countBut++;
+            }
+        }
+        sound /= countBut;
+        TIM_SetCompare1(TIM3, sound);  // Задаём заполнение
+        while (count - prev < 1) { }         // Ждём 
+    }
+}
+
+static void scan() {
+    uint32_t numBut = (GPIO_ReadInputData(GPIOA) & 0xFE) >> 1;  // Сохраняем состояния кнопок PA1-PA7
+    numBut |= (GPIO_ReadInputData(GPIOC) & GPIO_Pin_4) << 3;    // Сохраняем состояние кнопки PC4
+    if (numBut > 0) {
+        play(numBut);  // Проигрываем соответсвующие звуки
+    }
+    TIM_SetCompare1(TIM3, 0);
+}
+
+int main(void)
+{
+    __disable_irq();
+    
+    // PC6 - динамик, PA1-PA7, PC4 - клавиатура
+    // Подаём тактирование на порты А, С
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    
+    // Настраиваем ножки для клавиатуры
+    GPIO_InitTypeDef pinBut;
+    pinBut.GPIO_Mode = GPIO_Mode_IPD;
+    pinBut.GPIO_Pin  =  GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4;
+    pinBut.GPIO_Pin |= (GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7);
+    GPIO_Init(GPIOA, &pinBut);
+    pinBut.GPIO_Pin = GPIO_Pin_4;
+    GPIO_Init(GPIOC, &pinBut);
+    
+    // Настраиваем ножку PC6 для динамика
+    GPIO_InitTypeDef pinDyn;
+    pinDyn.GPIO_Mode = GPIO_Mode_AF_PP;
+    pinDyn.GPIO_Speed = GPIO_Speed_2MHz;
+    pinDyn.GPIO_Pin = GPIO_Pin_6;
+    GPIO_Init(GPIOC, &pinDyn);
+    
+    // Переназначаем ножку динамика
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+    GPIO_PinRemapConfig(GPIO_FullRemap_TIM3, ENABLE);
+    
+    // Подаём тактирование
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+    
+    // Настраиваем таймер
+    TIM_TimeBaseInitTypeDef timeBaseInitTypeDef;
+    TIM_TimeBaseStructInit(&timeBaseInitTypeDef);
+    timeBaseInitTypeDef.TIM_Prescaler = 1;
+    timeBaseInitTypeDef.TIM_Period = period;
+    TIM_TimeBaseInit(TIM3, &timeBaseInitTypeDef);
+    
+    // Настраиваем таймерный канал
+    TIM_OCInitTypeDef ocInitTypeDef;
+    TIM_OCStructInit(&ocInitTypeDef);
+    ocInitTypeDef.TIM_OCMode = TIM_OCMode_PWM1;
+    ocInitTypeDef.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OC1Init(TIM3, &ocInitTypeDef);
+    
+    // Заполняем массив синусов
+    fillSin(sinArr, sizeSinArr);
+    
+    __enable_irq();
+    
+    // Вызываем прерываниия каждые 6 мкс
+    SysTick_Config(SystemCoreClock / (1000 * 1000) * freqIntr_mcs); 
+    
+    // Запускаем таймер
+    TIM_Cmd(TIM3, ENABLE);
+    
+    while(1) {
+        scan();
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+#ifdef USE_FULL_ASSERT
+
+// эта функция вызывается, если assert_param обнаружил ошибку
+void assert_failed(uint8_t * file, uint32_t line)
+{ 
+    /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+     
+    (void)file;
+    (void)line;
+
+    __disable_irq();
+    while(1)
+    {
+        // это ассемблерная инструкция "отладчик, стой тут"
+        // если вы попали сюда, значит вы ошиблись в параметрах вызова функции из SPL. 
+        // Смотрите в call stack, чтобы найти ее
+        __BKPT(0xAB);
+    }
+}
+
+#endif
